fd69e5f436bffe66b8f968fc17982012e14760b8
Evan BOISSONNOT <evan@boissonnot.fr>
2023-05-30 00:31:21 +0200

    feat: deconstruct

---
+++ WebAPIMore/DiscoverDeconstruct/Posiition.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverRecords
+{
+    internal class PositionC
+    {
+        public PositionC(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = this.X;
+            y = this.Y;
+        }
+    }
+
+    internal struct PositionS 
+    {
+        public PositionS(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    internal record PositionR(int X, int Y);
+
+    //internal record class PositionRC(int X, int Y);
+
+    internal record class PositionRC
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    // internal record struct PositionRS(int X, int Y);
+    internal record struct PositionRS
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ WebAPIMore/DiscoverDeconstruct/Program.cs
@@ -0,0 +1,10 @@
+﻿using DiscoverRecords;
+
+PositionC position = new(1, 2);
+(var x, var y) = position;
+
+PositionR position2 = new(1, 2);
+(var xr, var yr) = position2;
+
+//PositionS position3 = new(1, 2);
+//(var xs, var ys) = position3;
