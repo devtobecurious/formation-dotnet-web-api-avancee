96a7a3c0bbd73057d6e76e5cf6f548f769ad0f22
Evan BOISSONNOT <evan@boissonnot.fr>
2023-05-29 23:21:20 +0200

    feat: init

---
+++ WebAPIMore/DiscoverRecords/Position.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverRecords
+{
+    internal class PositionC
+    {
+        public PositionC(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; init; }
+        public int Y { get; init; }
+    }
+
+    internal struct PositionS 
+    {
+        public PositionS(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    internal record PositionR(int X, int Y);
+
+    //internal record class PositionRC(int X, int Y);
+
+    internal record class PositionRC
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    // internal record struct PositionRS(int X, int Y);
+    internal record struct PositionRS
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
