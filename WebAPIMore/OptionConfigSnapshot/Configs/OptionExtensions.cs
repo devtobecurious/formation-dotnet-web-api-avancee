5ac33fe1ca555f386aea47e2495609be089805ac
Evan BOISSONNOT <evan@boissonnot.fr>
2023-06-01 16:42:29 +0200

    feat: prepa config snapshot

---
+++ WebAPIMore/OptionConfigSnapshot/Configs/OptionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OptionConfig.Configs
+{
+    public static class OptionExtensions
+    {
+        public static IServiceCollection AddSpecificsOptions(this IServiceCollection service, IConfiguration config)
+        {
+            var section = config.GetSection("Wookiee");
+            service.Configure<Wookiee>(section);
+
+            return service;
+        }
+    }
+}
