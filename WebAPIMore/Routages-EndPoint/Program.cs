51321c5739679a3815e10262d2205c3d9c6a13ae
Evan BOISSONNOT <evan@boissonnot.fr>
2023-06-03 12:16:57 +0200

    feat: endpoint

---
+++ WebAPIMore/Routages-EndPoint/Program.cs
@@ -0,0 +1,60 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
+//app.MapControllers();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Use(async (context, next) =>
+{
+    var currentEndpoint = context.GetEndpoint();
+
+    if (currentEndpoint is null) // toujours null car on appelle aucun RoutingMiddleWare
+    {
+        await next(context);
+        return;
+    }
+});
+
+//app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"2. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+    await next(context);
+});
+
+app.MapGet("/", (HttpContext context) =>
+{
+    Console.WriteLine($"3. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+    return "Hello World!";
+}).WithDisplayName("Hello");
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"3. Endpoint: {context.GetEndpoint()?.DisplayName ?? "(null)"}");
+    await next(context);
+});
+
+app.Run();
