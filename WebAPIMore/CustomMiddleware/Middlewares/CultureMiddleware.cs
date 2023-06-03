a48c3397b05deebd841e1ddeefc887e350921d19
Evan BOISSONNOT <evan@boissonnot.fr>
2023-06-03 16:29:59 +0200

    feat: fin middelware

---
+++ WebAPIMore/CustomMiddleware/Middlewares/CultureMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace CustomMiddleware.Middlewares
+{
+    public class CultureMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public CultureMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var cultureQuery = context.Request.Query["culture"];
+            if (!string.IsNullOrWhiteSpace(cultureQuery))
+            {
+                var culture = new CultureInfo(cultureQuery!);
+
+                CultureInfo.CurrentCulture = culture;
+                CultureInfo.CurrentUICulture = culture;
+            }
+
+            // Call the next delegate/middleware in the pipeline.
+            await next(context);
+        }
+    }
+
+    public static class ExtensionMiddleware
+    {
+        public static IApplicationBuilder AddCultureDefinition(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CultureMiddleware>();
+        }
+    }
+}
---
+++ WebAPIMore/CustomMiddleware/Program.cs
@@ -0,0 +1,44 @@
+using CustomMiddleware.Middlewares;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+app.AddCultureDefinition();
+
+//app.Use(async (context, next) =>
+//{
+//    var cultureQuery = context.Request.Query["culture"];
+//    if (!string.IsNullOrWhiteSpace(cultureQuery))
+//    {
+//        var culture = new CultureInfo(cultureQuery!);
+
+//        CultureInfo.CurrentCulture = culture;
+//        CultureInfo.CurrentUICulture = culture;
+//    }
+
+//    // Call the next delegate/middleware in the pipeline.
+//    await next(context);
+//});
+
+app.Run();
