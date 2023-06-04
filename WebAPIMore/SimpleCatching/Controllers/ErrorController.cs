5f16c604a34262a8c5b1a51f7a1d37656e2034dc
Evan BOISSONNOT <evan@boissonnot.fr>
2023-06-04 18:16:35 +0200

    feat: exception management (default)

---
+++ WebAPIMore/SimpleCatching/Controllers/ErrorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCatching.Controllers
+{
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult HandleError() => Problem();
+    }
+}
--- WebAPIMore/SimpleCatching/Program.cs
+++ WebAPIMore/SimpleCatching/Program.cs
@@ -16,6 +16,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+if (app.Environment.IsProduction())
+{
+    app.UseExceptionHandler("/Error");
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
