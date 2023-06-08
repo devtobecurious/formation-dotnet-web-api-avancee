0f6633a241659f5261a34c255b88bf5fff4d1ce6
Evan BOISSONNOT <evan@boissonnot.fr>
2023-06-08 23:44:21 +0200

    feat: add one provider

--- WebAPIMore/LoggerBasicWithProvidersSelectOne/Program.cs
+++ WebAPIMore/LoggerBasicWithProvidersSelectOne/Program.cs
@@ -2,17 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Logging.ClearProviders();
-//builder.Logging.AddConsole(options =>
-//{
-//    options.DisableColors = false;
-//});
-builder.Logging.AddSimpleConsole(option =>
-{
-    option.SingleLine = true;
-    option.ColorBehavior = Microsoft.Extensions.Logging.Console.LoggerColorBehavior.Enabled;
-});
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
